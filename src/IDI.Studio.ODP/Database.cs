f02a9a8d310c8f69cb59399fc67f2c082fae2203
johnny <johnny.l.wang@hotmail.com>
2016-09-12 13:20:18 +0800

    init

---
+++ src/IDI.Studio.ODP/Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace IDI.Studio.ODP
+{
+    public sealed class Database
+    {
+        public static readonly Database Instance = new Database();
+
+        private string connectionString = string.Empty;
+
+        private Database()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Oracle"].ToString();
+        }
+
+        public List<T> Query<T>(string sql) where T : new()
+        {
+            var list = new List<T>();
+
+            using (var connection = new OracleConnection(connectionString))
+            {
+                var command = new OracleCommand(sql, connection);
+                command.CommandType = CommandType.Text;
+                command.CommandTimeout = 90;
+
+                using (var reader = command.ExecuteReader(CommandBehavior.Default))
+                {
+                    while (reader.Read())
+                    {
+
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
