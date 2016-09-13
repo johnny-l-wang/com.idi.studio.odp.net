81b3024938f7f2355aef9cc22fbda29a5f5cc353
johnny <johnny.l.wang@hotmail.com>
2016-09-13 13:39:10 +0800

    M

---
+++ src/IDI.Studio.ODP/DatabaseExtention.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace IDI.Studio.ODP
+{
+    internal static class DatabaseExtention
+    {
+        public static List<T> ToList<T>(this OracleDataReader reader) where T : new()
+        {
+            var list = new List<T>();
+
+            while (reader.Read())
+            {
+                list.Add(reader.ToObject<T>());
+            }
+
+            return list;
+        }
+
+        private static T ToObject<T>(this OracleDataReader reader) where T : new()
+        {
+            return new T();
+        }
+    }
+}
